393b2ca7d3a54a6b15a62a7189e2f23048157395
Brandon Weaver <a7xncob@gmail.com>
2020-07-30 20:33:34 -0400

    Add header.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+const Header = () => {
+	return (
+		<div className="Header">
+			<h1>Brandon M. Weaver</h1>
+		</div>
+	);
+}
+
+export default Header;
